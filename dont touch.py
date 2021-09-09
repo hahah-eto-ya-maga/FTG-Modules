4d33d4ae5deed13c14e7cec61ac18ef328d937a8
hahah-eto-ya-maga <66819081+hahah-eto-ya-maga@users.noreply.github.com>
2021-09-09 20:05:13 +0400

    Add files via upload

---
+++ dont touch.py
@@ -0,0 +1,37 @@
+# coded by Y4sperMaglot
+
+from telethon.tl.functions.account import UpdateProfileRequest
+from telethon import events, functions
+from asyncio import sleep
+from .. import loader, utils
+
+@loader.tds
+class SexMod(loader.Module):
+    """"""
+    strings = {'name': 'Гениальное - просто'}
+
+    async def client_ready(self, client, db):
+        self.client = client
+        self.db = db
+
+    async def anamecmd(self, message):
+        """"""
+        emojis = ['👉', '👈']
+        name = 'Фернандо Санчес'
+
+        if not self.db.get("NameEmoji", "status", False):
+            self.db.set("NameEmoji", "status", True)
+            await message.edit("<b>Активировано</b>")
+            while self.db.get("NameEmoji", "status", True):
+                await self.client(functions.account.UpdateProfileRequest(first_name=f'{name} {emojis[0]}{emojis[1]}‍‌‌‌‌‍‌‌‌‌‌‌‌‍‍‍‍‍‍‍‍‍‍‍', last_name=''))
+                await sleep(5)
+                await self.client(functions.account.UpdateProfileRequest(first_name=f'{name} {emojis[0]}    {emojis[1]}‍‌‌‌‌‍‌‌‌‌‌‌‌‍‍‍‍‍‍‍‍‍‍‍', last_name=''))
+                await sleep(5)
+
+
+
+        else:
+            self.db.set("NameEmoji", "status", False)
+            await message.edit("<b>Деактивировано</b>")
+            await self.client(functions.account.UpdateProfileRequest(first_name=f'{name}‍‌‌‌‌‍‌‌‌‌‌‌‌‍‍‍‍‍‍‍‍‍‍‍', last_name=''))
+
