3de465d93aa17adfeed01303daf08a60ea214c95
hahah-eto-ya-maga <66819081+hahah-eto-ya-maga@users.noreply.github.com>
2021-09-09 16:11:45 +0400

    Add files via upload

---
+++ gule.py
@@ -0,0 +1,11 @@
+from .. import loader, utils
+
+
+@loader.tds
+class ThousandMinusSevenMod(loader.Module):
+    async def gulecmd(self, message):
+        thousand = 1000
+        while thousand > 0:
+            thousand = thousand - 7
+            await message.edit(f'{str(thousand)} - 7')
+        await message.edit('<b>Я гуль...</b>')
