ca6bb62cdeb11c9e877c76504e81dd68cf53af1c
hahah-eto-ya-maga <66819081+hahah-eto-ya-maga@users.noreply.github.com>
2021-09-21 16:26:04 +0400

    Add files via upload

---
+++ NewYear.py
@@ -0,0 +1,18 @@
+from .. import loader
+import datetime
+from time import strftime
+
+@loader.tds
+class DaysDateMod(loader.Module):
+    """Сколько там до нового года"""
+    strings = {'name': 'Ждем новый год всем фтг комьюнити'}
+
+    async def nycmd(self, message):
+        """.ny"""
+        now = datetime.datetime.today()
+        NY = datetime.datetime(int(strftime('%Y')) + 1, 1, 1)
+        d = NY - now
+        mm, ss = divmod(d.seconds, 60)
+        hh, mm = divmod(mm, 60)
+        result = '❄ До нового года: {} дн. {} час. {} мин. {} сек.'.format(d.days, hh, mm, ss)
+        await message.edit(result)
