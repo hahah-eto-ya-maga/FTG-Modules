ed7cc5b9f3165e3f5aa6d8980f845a57d6cd3e89
hahah-eto-ya-maga <66819081+hahah-eto-ya-maga@users.noreply.github.com>
2021-09-24 20:40:27 +0400

    Add files via upload

---
+++ PassGenerator.py
@@ -0,0 +1,21 @@
+
+from .. import loader, utils
+from random import choice as рандомыч
+@loader.tds
+class PassGeneratorMod(loader.Module):
+    """Генератор паролей"""
+    strings = {'name': 'Генератор Паролей'}
+    async def passgencmd(self, message):
+        """.passgen количество символов"""
+        args = utils.get_args_raw(message)
+        try:
+            simvoli = int(args)
+        except:
+            await message.edit('<b>Введит число.</b>')
+            return
+
+        parol = ''
+        for x in range(simvoli):
+            parol = parol + рандомыч(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+
+        await message.edit(parol)
