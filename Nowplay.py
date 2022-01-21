442451b75038a162e15da3da6b384c005da07d03
Y4sper <66819081+hahah-eto-ya-maga@users.noreply.github.com>
2022-01-21 10:28:49 +0400

    Add files via upload

--- Nowplay.py
+++ Nowplay.py
@@ -12,8 +12,8 @@ class NowPlayMod(loader.Module):
         """Скидывает то, что сейчас играет."""
         await message.edit('Минуточку...')
         np = await message.client.inline_query('nowplaybot', '')
-	  if message.chat is not None:
-	      await message.client.send_file(message.to_id, np[0].document)
+        if message.chat is not None:
+            await message.client.send_file(message.to_id, np[0].document)
         else:
-	     await message.client.send_file(message.to_id, np[0].result.document)
-	 await message.delete()
+        	await message.client.send_file(message.to_id, np[0].result.document)
+	    await message.delete()
