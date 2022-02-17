5ecde8012ea79506be897a42c75b76a898033d67
Y4sper <66819081+hahah-eto-ya-maga@users.noreply.github.com>
2022-02-17 20:07:07 +0400

    Update Nowplay.py

--- Nowplay.py
+++ Nowplay.py
@@ -12,8 +12,8 @@ class NowPlayMod(loader.Module):
         """Скидывает то, что сейчас играет."""
         await message.edit('Шо я щас слушаю...')
         np = await message.client.inline_query('nowplaybot', '')
-        if message.chat is not None:
+        try:
         	await message.client.send_file(message.to_id, np[0].document)
-        else:
+        except:
         	await message.client.send_file(message.to_id, np[0].result.document)
         await message.delete()
